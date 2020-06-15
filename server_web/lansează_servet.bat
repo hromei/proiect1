89fe6b0b01c90367d36aae6e29bd3e9708c1eabb
hromei <61143705+hromei@users.noreply.github.com>
2020-06-15 18:59:35 +0300

    Add files via upload

---
+++ server_web/lansează_servet.bat
@@ -0,0 +1,11 @@
+@echo Starting
+
+@echo This may take some time ...
+
+@cmd /k "c:\Users\andre\proiect1-hromei\server_web\lanseaza_server.bat" start
+
+echo Loading completed
+
+start http://localhost:5678
+
+@pause
